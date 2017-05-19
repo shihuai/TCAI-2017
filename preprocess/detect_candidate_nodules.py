8ad2612b6bcb2ad407d24df2665ef5bc57ac504a
shihuai <liubanggui92@163.com>
2017-05-19 11:55:18 +0800

    fixed the detect file

--- preprocess/detect_candidate_nodules.py
+++ preprocess/detect_candidate_nodules.py
@@ -15,100 +15,98 @@ import os
 import math
 
 
+def BFS(masks):
+    map3d = masks.copy()
+    ch, height, width = masks.shape
+    visited = np.ones(masks.shape, dtype = np.uint8)
+    visited[masks == 255.] = 0
+    steps = [[1, 0, 0], [-1, 0, 0], [0, 1, 0], [0, -1, 0], [0, 0, 1], [0, 0, -1]]
+    count = 1
+
+    for z in range(ch):
+        if np.any(map3d[z] == 255.):
+            yx = np.where(map3d[z] == 255.)
+            coord = [z, yx[1][0], yx[0][0]]
+            queue = []
+            queue.append(coord)
+            visited[coord[0], coord[2], coord[1]] = 1.
+
+            while len(queue) > 0:
+                coord = queue[0]
+                del queue[0]
+
+                for step in steps:
+                    z = coord[0] + step[0]
+                    y = coord[2] + step[2]
+                    x = coord[1] + step[1]
+                    if (z >= 0 and z < ch) and (y >= 0 and y < height) \
+                            and (x >= 0 and x < width) and (visited[z, y, x] == 0):
+                        visited[z, y, x] = 1
+                        map3d[z, y, x] = count
+                        queue.append([z, y, x])
+
+            count += 1
+
+    return map3d, count
+
 #获取阈值大于255.的位置的点作为结节内部的点
 def get_coords(mask, threshold):
 
-    print mask.shape
+    map3d, nodule_num = BFS(mask)
+    ch, height, width = map3d.shape
+
     coords = []
-    index_list = np.where(mask >= 255.)
-    first_axis = index_list[0]
+    for num in range(1, nodule_num):
+        for z in range(1, ch - 1):
+            if np.any(map3d[z] == num) and np.any(map3d[z + 1] != num):
+                if np.any(map3d[z - 1] != num):
+                    temp = map3d[z]
+                    temp[temp == num] = 0.
+                    map3d[z] = temp
+                    break
 
-    for idx, i in enumerate(first_axis):
-        coords.append(np.array([index_list[2][idx], index_list[1][idx], index_list[0][idx]]))
-
-    #for z in range(mask.shape[0]):
-    #    for y in range(mask.shape[1]):
-    #        for x in range(mask.shape[2]):
-    #            if (mask[z, y, x] == 255.):
-    #                coords.append(np.array([x, y, z]))
+    for num in range(1, nodule_num):
+        index_list = np.where(map3d == num)
+        if len(index_list[0]) > 0:
+            temp_coord = []
+            first_axis = index_list[0]
+            for idx, i in enumerate(first_axis):
+                temp_coord.append(np.array([index_list[2][idx], index_list[1][idx], index_list[0][idx]]))
+            coords.append(temp_coord)
 
     coords = np.array(coords)
     #print 'candidate coords shape: ', coords.shape
 
     return coords
 
-#计算两个空间坐标之间的欧式距离
-def euclidean_dist(point1, point2):
-    A = point1 - point2
-    B = A * A
-
-    return math.sqrt(sum(B))
-
-#对候选结节内部的点进行聚类，如果两两位置间的距离小于30.0mm，
-#即这两个点位于同一个结节内
-def cluster_coords(coords, visited):
-    count = 1
-
-    for k in range(coords.shape[0]):
-        if not visited[k]:
-            visited[k] = count
-
-            for idx in range(k + 1, coords.shape[0]):
-                if visited[idx] == 0 and euclidean_dist(coords[idx, :], coords[k, :]) <= 50.0:
-                    visited[idx] = count
-
-            count += 1
-
-    #计算候选结节的中心位置坐标
+def get_centroids(coords):
     cluster_centroids = []
-    cluster_num = []
-    for label in range(1, count):
-        centroid = [0.0, 0.0, 0.0]
-        num = 0
-        for idx in range(coords.shape[0]):
-            if visited[idx] == label:
-                centroid[0] += coords[idx, 0]
-                centroid[1] += coords[idx, 1]
-                centroid[2] += coords[idx, 2]
-                num += 1
+    print coords.shape
 
-        centroid[0] /= num
-        centroid[1] /= num
-        centroid[2] /= num
+    for nodule in coords:
+        num = len(nodule)
+        x = 0.0
+        y = 0.0
+        z = 0.0
 
-        cluster_num.append(num)
-        cluster_centroids.append(centroid)
+        for coord in nodule:
+            x += coord[0]
+            y += coord[1]
+            z += coord[2]
+
+        x = (1.0 * x) / num
+        y = (1.0 * y) / num
+        z = (1.0 * z) / num
+
+        cluster_centroids.append([x, y, z])
 
     cluster_centroids = np.array(cluster_centroids)
-
-    print 'have %d clusters.' % (count - 1)
-
-    return cluster_centroids, cluster_num, visited
-
-#过滤掉小的候选结节
-def filter_centroids(coords, cluster_centroids, cluster_num, visited):
-    new_cluster_centroids = []
-
-    for label in range(1, len(cluster_num)):
-        count = 0
-
-        for idx in range(coords.shape[0]):
-            if visited[idx] == label and  \
-                    euclidean_dist(coords[idx], cluster_centroids[label - 1]) >= 0.5:
-                count += 1
-
-        if count >= cluster_num[label - 1] / 3:
-            new_cluster_centroids.append(cluster_centroids[label - 1])
-
-    new_cluster_centroids = np.array(new_cluster_centroids)
-
-    print 'After filter, the rest cluster centroid is: ', new_cluster_centroids.shape
-
-    return new_cluster_centroids
+    print 'have {} candidate nodules.'.format(cluster_centroids.shape[0])
+    return cluster_centroids
 
 #获得候选结节
 def get_candidate_nodule_coords():
-    file_list = glob('./output/test_masks/CT_mask/*.npy')
+    file_list = glob('../output/test_masks/CT_mask/*.npy')
     num = len(file_list)
 
     candidate_nodules = {}
@@ -116,15 +114,12 @@ def get_candidate_nodule_coords():
         mask = np.load(patient)
 
         coords = get_coords(mask, 0.5)
-        visited = np.zeros(coords.shape[0])
-        cluster_centroids, cluster_num, visited = cluster_coords(coords, visited)
-
-        #cluster_centroids = filter_centroids(coords, cluster_centroids, cluster_num, visited)
+        cluster_centroids = get_centroids(coords)
 
         split_path = patient.split('/')[-1]
         name = split_path.split('.')[0]
 
-        mhd_path = './data/sample_patients/val/' + name + '.mhd'
+        mhd_path = '../data/sample_patients/test/' + name + '.mhd'
         itk_img = sitk.ReadImage(mhd_path)
         origin = np.array(itk_img.GetOrigin())
         spacing = np.array(itk_img.GetSpacing())
@@ -137,7 +132,7 @@ def get_candidate_nodule_coords():
         print ('{}/{} have preprocessed: {}'.format(idx, num, patient))
 
     #保存候选结节的中心坐标
-    fwrite = open('./output/test_masks/candidate_nodules_centroids.csv', 'w')
+    fwrite = open('../output/test_masks/candidate_nodules_centroids.csv', 'w')
     for key in candidate_nodules.keys():
         nodules_centroid = candidate_nodules[key]
         for coord in nodules_centroid:
